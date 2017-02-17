2a22bb4b9b5713a045da78cac65e635feb06a831
sylvia villegas <sylvia@svillegas.com>
2017-02-17 15:55:57 -0500

    Create <li> for each candidate returned

--- election.js
+++ election.js
@@ -4,6 +4,17 @@ $(document).ready(function() {
     url: 'https://bb-election-api.herokuapp.com/',
     method: 'GET',
     dataType: 'json'
+  }).done(function(responseData) {
+    // unpack responseData...
+    var candidates = responseData.candidates;
+
+    //
+    $.each(candidates, function(index, data) {
+      var listItem = $('<li>');
+      $('<h3>').html(data.name).appendTo(listItem);
+      $('<span>').html(data.votes).appendTo(listItem);
+      $('#candidates').append(listItem);
+    })
   });
 
 });
